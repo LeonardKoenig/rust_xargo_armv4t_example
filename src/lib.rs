b8d1815d417239341d9830492927f3542fbaa69e
Leonard König <leonard.r.koenig@googlemail.com>
2016-10-24 19:45:25 +0200

    example code for calling C and getting called

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 #![no_std]
 #![feature(lang_items)]
+//extern crate libc;
 
 #[lang="panic_fmt"]
 extern fn panic_fmt(_: ::core::fmt::Arguments, _: &'static str, _: u32) -> ! {
         loop {}
+}
+
+extern {
+    fn color_screen();
+}
+
+#[no_mangle]
+pub extern "C" fn rust_call()
+{
+    unsafe { color_screen(); }
 }
